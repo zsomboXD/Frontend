4133ed9415fe47bdcec0c74c9c9cc6b0826844d8
zsomboXD <bozsombo@gmail.com>
2025-02-27 12:46:41 +0100

    message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {  Collapse,  Navbar,  NavbarToggler,  NavbarBrand,  Nav,  NavItem,   UncontrolledDropdown,  DropdownToggle,  DropdownMenu, DropdownItem,  NavbarText,} from 'reactstrap';
 import { useState } from "react";
@@ -7,109 +6,94 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { useEffect } from "react";
 import { extractUrlAndId } from "../utility/utils";
+import { Outlet, NavLink } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 
 export const Header=()=> {
-  const [isOpen, setIsOpen]=useState(false);
-  const [avatar,setAvatar]=useState(null)
+  const [isOpen, setIsOpen] = useState(false);
   const {user,logoutUser}=useContext(UserContext)
+  const [avatar,setAvatar]=useState(null)
 
   useEffect(()=>{
     user?.photoURL && setAvatar(extractUrlAndId(user.photoURL).url)
     !user && setAvatar(null)
   },[user,user?.photoURL])
 
+  const fokep = "https://res.cloudinary.com/paksiblog13/image/upload/v1736880697/noFilter_uatjfq.png";
   const toggle = () => setIsOpen(!isOpen);
 
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        WorkOutwise
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Home</Nav.Link>
-          <Nav.Link href="#pricing">About Us</Nav.Link>
-          <NavDropdown title="Product" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-          <NavDropdown title="Article" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+    <div>
+    <Navbar fixed='top' expand="md" 
+    className="menu"
+    style={{borderBottom:'1px solid gray',backgroundColor:'lightgray'}} >
+      <NavbarBrand href="/">
+      
+      <img src={fokep}
+      style={{
+        width: '100%',
+        height: '36px',
+        objectFit: 'cover',
+      }}
+    />
+      
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="me-auto" navbar>
+          <NavItem>
+            <NavLink className="nav-link" to='/'>Főoldal</NavLink>
+          </NavItem>
 
-          <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink className="nav-link" to='/'>Főoldal</NavLink>
-            </NavItem>
-
-            <NavItem>
-              <NavLink className="nav-link" to='/posts'>Posztok</NavLink>
-            </NavItem>
-            {user && 
-             <NavItem>
-              <NavLink className="nav-link" to='/create'>Új poszt</NavLink>
-            </NavItem>
-            }
-
-          
-          </Nav>
-          <Nav navbar>
-          { !user ? 
-          <>
-            <NavItem>
-              <NavLink className="nav-link" to='/auth/in'>Belépés</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to='/auth/up'>Regisztráció</NavLink>
-            </NavItem>
-          </> 
-          :
-          <>
-            <NavItem>
-              <NavLink className="nav-link" to='/'
-                onClick={()=>logoutUser()}
-                >Kijelentkezés
-              </NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-               {avatar ? <img className='myavatar' src={avatar}/> : <RxAvatar title={user.displayName}/>}
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem>
-                  <NavLink className="nav-link" to='/profile'> Személyes adatok</NavLink> 
-                </DropdownItem>
-                <DropdownItem>
-                  <NavLink className="nav-link" to='/deleteAccount'> Felhasználói fiók törlése </NavLink>    
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </>
+          <NavItem>
+            <NavLink className="nav-link" to='/posts'>Posztok</NavLink>
+          </NavItem>
+          {user && 
+           <NavItem>
+            <NavLink className="nav-link" to='/create'>Új poszt</NavLink>
+          </NavItem>
           }
-          </Nav>  
-        </Collapse>
 
+        
         </Nav>
-      </Navbar.Collapse>
+        <Nav navbar>
+        { !user ? 
+        <>
+          <NavItem>
+            <NavLink className="nav-link" to='/auth/in'>Belépés</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to='/auth/up'>Regisztráció</NavLink>
+          </NavItem>
+        </> 
+        :
+        <>
+          <NavItem>
+            <NavLink className="nav-link" to='/'
+              onClick={()=>logoutUser()}
+              >Kijelentkezés
+            </NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+             {avatar ? <img className='myavatar' src={avatar}/> : <RxAvatar title={user.displayName}/>}
+            </DropdownToggle>
+            <DropdownMenu end>
+              <DropdownItem>
+                <NavLink className="nav-link" to='/profile'> Személyes adatok</NavLink> 
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink className="nav-link" to='/deleteAccount'> Felhasználói fiók törlése </NavLink>    
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </>
+        }
+        </Nav>  
+      </Collapse>
     </Navbar>
+    <Outlet />
+  </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+
+            <UserProvider>
+                <App />
+            </UserProvider>
+
 )
